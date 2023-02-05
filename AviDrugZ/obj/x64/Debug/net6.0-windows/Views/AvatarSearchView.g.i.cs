3cd756ef60ae7144ae0507ebe9837dc4778fc860
Klauensprung <36606771+Klauensprung@users.noreply.github.com>
2023-02-05 22:48:57 +0100

    removed binary

--- AviDrugZ/obj/x64/Debug/net6.0-windows/Views/AvatarSearchView.g.i.cs
+++ AviDrugZ/obj/x64/Debug/net6.0-windows/Views/AvatarSearchView.g.i.cs
@@ -54,7 +54,7 @@ namespace AviDrugZ.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AviDrugZ;component/views/avatarsearchview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AviDrugZ;V1.0.0.0;component/views/avatarsearchview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\AvatarSearchView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
